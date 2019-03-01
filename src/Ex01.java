3af22b578248c8d265a0e3fa59ebdaf30526d1ef
Marcelo Afanaci <m.afanaci@gmail.com>
2019-02-28 22:04:25 -0300

    Lendo nome do usuario

--- src/Ex01.java
+++ src/Ex01.java
@@ -5,6 +5,8 @@ public class Ex01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Digite seu nome");
+		
+		String nome = scanner.nextLine();
 
 	}
 
