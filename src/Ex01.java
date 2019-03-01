f9709186cad63ec5fb3524f6c08d4d9679077ac3
Marcelo Afanaci <m.afanaci@gmail.com>
2019-02-28 22:03:19 -0300

    Adicionando classe para leitura de inputs do usuario

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite seu nome");
 
 	}
 
